e3243a71577a7905b8ecde7135917b87b5e2e74d
marlmeR <91583401+marlmeR@users.noreply.github.com>
2022-10-31 09:04:03 +0700

    Add files via upload

---
+++ OOP_RAIN_NAUFAL_1202210400_MODUL1/modul1/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Selamat Datang di Restoran EAD\nSilahkan Register Terlebih Dahulu");
+        System.out.println("=======================");
+        System.out.println("Nama: Naufal_SI4505");
+        System.out.println("No. Handphone: 1202210400");
+        System.out.println();
+        System.out.println("Register Success");
+        System.out.println("Name: Naufal_SI4505");
+        System.out.println("Phone Number: 1202210400");
+        System.out.println("=======================");
+        System.out.println("1.Menu");
+        System.out.println("2.Pilih Menu");
+        System.out.println("3.Keluar");
+        System.out.println("=======================");
+        System.out.println("Masukkan pilihan: 1");
+        System.out.println("Menu");
+        System.out.println("=======================");
+    }
+}
